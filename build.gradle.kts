f58cc0f4915df13ecf6c1290694d270bbf301a96
Andrew Something <corstenmm@gmail.com>
2022-01-02 19:04:57 +0700

    some updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     java
-    id("org.springframework.boot") version "2.4.3"
+
+    id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("io.freefair.lombok") version "5.3.0"
+    id("io.freefair.lombok") version "6.3.0"
 }
 
 group = "org.example"
@@ -13,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
@@ -20,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
-    runtimeOnly("com.h2database:h2:1.4.200")
-
-    implementation("org.springdoc:springdoc-openapi-ui:1.5.5")
+    // Other
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.3")
+    runtimeOnly("com.h2database:h2")
 }
